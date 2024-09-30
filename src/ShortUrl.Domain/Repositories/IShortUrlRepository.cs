de73137d68d864362063c2ab20cfcf44a3dd8e45
mohammed khairelseed <mohammed_494@outlook.com>
2024-09-30 16:26:19 +0300

    add shorturl repository

---
+++ src/ShortUrl.Domain/Repositories/IShortUrlRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShortUrl.Domain.Entities;
+using ShortUrl.Domain.SharedKernel;
+
+namespace ShortUrl.Domain.Repositories
+{
+    public interface IShortUrlRepository :IRepository<Shorturl>
+    
+    {
+        
+    }
+}
